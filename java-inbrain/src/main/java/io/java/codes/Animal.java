caf43fd89adb1aa132676ab19ff359899f985124
1kevinson <kevinkouomeu@icloud.com>
2020-09-28 22:36:22 +0200

    Exceptions

--- java-inbrain/src/main/java/io/java/codes/Animal.java
+++ java-inbrain/src/main/java/io/java/codes/Animal.java
@@ -1,5 +1,11 @@
 package io.java.codes;
 
 public class Animal {
+    void talk() throws ArithmeticException {
+        try {
+            throw new ArithmeticException();
+        }catch (ArithmeticException exception){
 
+        }
+    }
 }
