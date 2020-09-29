6a6a0711d567d2a7873a7ae472a0ec92e2cf68ff
1kevinson <kevinkouomeu@icloud.com>
2020-09-29 06:01:19 +0200

    Add Junit Jupiter 5

--- java-inbrain/src/main/java/Application.java
+++ java-inbrain/src/main/java/Application.java
@@ -1,19 +1,6 @@
-import io.java.algorithms.PrintPattern;
-import io.java.codes.Animal;
-import io.java.codes.Dog;
-
-import java.util.regex.Pattern;
-
 public class Application implements Cloneable {
 
     public static void main(String... args) {
-       PrintPattern.getPrint();
-        Dog d = new Dog();
-        Animal e = new Dog();
-
-        System.out.println(Pattern.matches(".(w)+s","esijdfis"));
-        System.out.println(d instanceof Animal);
-        System.out.println(e instanceof Animal);
     }
 }
 
