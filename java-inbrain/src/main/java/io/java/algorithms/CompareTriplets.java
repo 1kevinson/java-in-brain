a0591211ed4f57d756abbdc5ad6492b43a9e622a
Arsene Kevin <kevinkouomeu@icloud.com>
2020-10-03 18:06:45 +0200

    Correct SimpleArraySum Test

--- java-inbrain/src/main/java/io/java/algorithms/CompareTriplets.java
+++ java-inbrain/src/main/java/io/java/algorithms/CompareTriplets.java
@@ -6,13 +6,11 @@ import java.util.Map;
 public class CompareTriplets {
 
     public Map<String, Integer> getComparison(int[] a, int[] b) {
-        a = new int[3];
-        b = new int[3];
         int aPoints = 0;
         int bPoints = 0;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] < 0  || b[i] < 0|| a[i] > 100 || b[i] > 100) {
+            if (a[i] < 1 || b[i] < 1 || a[i] > 100 || b[i] > 100) {
                 throw new ArrayIndexOutOfBoundsException();
             }
 
--- java-inbrain/src/test/java/io/java/algorithms/CompareTripletsTest.java
+++ java-inbrain/src/test/java/io/java/algorithms/CompareTripletsTest.java
@@ -1,6 +1,23 @@
 package io.java.algorithms;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Compare 2 triplets arrays")
 public class CompareTripletsTest {
 
+    private final CompareTriplets compareTriplets = new CompareTriplets();
+
+    @Test
+    @DisplayName("Check that 1 <= a[i] <= 100 ")
+    void checkThatElementInArgumentGreaterThan0AndSmallerThan100() {
+
+    }
+
+    @Test
+    @DisplayName("Check that comparison of two array if OK")
+    void checkThatComparisonIsCorrect() {
+
+    }
 
 }
